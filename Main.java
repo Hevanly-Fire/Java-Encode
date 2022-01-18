bfd76aa66d79038e073418c79aa8c9b35726d3d9
abhijeet chatterjee <>
2022-01-18 14:35:33 +0000

    just added a cmnt in main.java and trying commit and push func.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+/*Dont delete this file or the whole project will crash(TRIED ALREADY :) ) This is the only file that is executed when you run the program. */
 public class Main
 {
   public static void main(String args[]) throws Exception
---
+++ enc.java
@@ -0,0 +1,15 @@
+import java.util.*;
+//this program is to encode any string
+public class enc
+{
+  String en;
+    public enc()
+    {
+      Scanner sc = new Scanner(System.in);
+      en = sc.nextLine();
+    }
+    public static void main(String Args[])
+    {
+
+    }
+}
